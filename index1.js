a9d3d15e2cf271e9604a646e06725f4fd2b4b9ef
Kirti Namdeo Pawar <aaradhyapawar0508@gmail.com>
2024-03-15 12:45:34 +0530

    home page change

--- index1.js
+++ index1.js
@@ -2,3 +2,19 @@ var q1 = document.querySelector(".card");
 q1.addEventListener("click", function () {
   document.body.style.backgroundImage = 'url("confetti.jpg")';
 });
+/*const  btn64= document.getElementsByClassName('button-64 ');
+  const btnGroup1 = document.getElementsById('btnGroup');
+
+  btn64.addEventListener('click', () => {
+    btn64.style.display = 'none';
+    btnGroup1.style.display = 'block'; 
+   });*/
+   function showButtons() {
+    document.querySelector('.button-64').style.display = 'none';
+    document.getElementById('button1').classList.remove('hidden');
+    document.getElementById('button2').classList.remove('hidden');
+    document.getElementById('button1').classList.add('highlighted');
+    document.getElementById('button2').classList.add('highlighted');
+  }
+    
+    
